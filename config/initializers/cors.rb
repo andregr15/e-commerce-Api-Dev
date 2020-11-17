43ffc9da4769409dbcaad74e063cdf0cd21600d5
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2020-11-17 07:16:00 -0300

    Exposing header fields to authentication on rack cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete]
+    resource '*', 
+      headers: :any, 
+      expose: ['access-token', 'uid', 'expiry', 'client', 'token-type'], 
+      methods: [:get, :post, :put, :patch, :delete]
   end
 end
